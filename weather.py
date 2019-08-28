265eeaaa69bd6b81b9f89a20a60b1e73d2111553
Sam <snmriley@gmail.com>
2019-08-28 13:36:58 +0800

    Debug attempt re: unreliable data collection

--- weather.py
+++ weather.py
@@ -44,15 +44,40 @@ def writeData(row):
 	ser.write("y")
 	time.sleep(1)
 	dataIn = ser.readline().strip()
+	writet("data1: ")
+	print(dataIn)
 	wks.update_cell(row, TEMP_COL, dataIn)
 	time.sleep(1)
 	dataIn = ser.readline().strip()
+	writet("data2: ")
+	print(dataIn)
 	wks.update_cell(row, PRESSURE_COL, dataIn)
 	time.sleep(1)
 	dataIn = ser.readline().strip()
+	writet("data3: ")
+	print(dataIn)
 	wks.update_cell(row, HUMIDITY_COL, dataIn)
 
 	return 1;
+
+#debugging command. To be called manually with debug()
+def takeData():
+	seriousBS = ser.readline().strip()
+	return seriousBS
+#debugging command. To be called manually with debug()
+def sendYes():
+	ser.write("y")
+	return 1
+#debugging command. To enable calling of takeData and send "yes"
+def debug():
+	while True:
+		input = raw_input("TYPE s FOR SEND A \"YES\", TYPE t FOR TAKE DATA")
+		if input=="s":
+			sendYes()
+		elif input=="t":
+			printt(takeData())
+		else:
+			printt("didnt catch that?") 
 
 #reauthorise the google sheet to use
 def reauthoriseCreds():
@@ -65,7 +90,14 @@ def mainFunc():
 	reauthoriseCreds()
 	currentRow = next_free_row(wks)
 	theDate = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-	wks.update_cell(currentRow, 1, theDate)
+	wks.update_cell(currentRow, 1, theDate)	
+
+	#check if the serial port is open, if not, reopen
+	if not ser.isOpen():
+		ser.open()
+		printt("re-opened serial port")
+	else:
+		printt("serial port waas gucci")
 	writeData(currentRow)
 
 	printt("Sheet write completed")
@@ -91,21 +123,19 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('auth.json', scop
 gc = gspread.authorize(credentials)
 
 wks = gc.open("Weather data").sheet1
-print("done")
+print("done authorising")
 
-#preparing the arduino
+#preparing the arduino. I think it's fucky because we aren't letting it start up before sending it commands, hence the sleep.
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+time.sleep(5)
+
 
 dTemp = ""
 dPressure = ""
 dHumidity = ""
 
 logging.basicConfig()
-
-#sched = BlockingScheduler()
-#sched.add_job(mainFunc, 'cron', hour='0-24')
-#sched.add_job(userInput)
 printt("Making a data collection. Waiting... ")
-#sched.start()
+#userInput()
 mainFunc()
-
+#debug()
